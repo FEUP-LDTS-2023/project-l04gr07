673228418b566d9ed7424e524027e821ecd8cbba
beatrizbernardoo <beatrizxalexandra@gmail.com>
2023-12-06 19:09:04 +0000

    Added NormalPlayerStateTest

---
+++ src/test/java/l04gr07/states/NormalPlayerStateTest.java
@@ -0,0 +1,46 @@
+package l04gr07.states;
+
+import l04gr07.control.PlayerController;
+import l04gr07.model.Game.FieldElements.Player;
+import l04gr07.model.Game.FieldElements.PlayerState.NormalPlayerState;
+import l04gr07.view.ElementsView.PlayerViewer;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class NormalPlayerStateTest {
+    @Test
+    void testGetViewer() {
+        NormalPlayerState normalPlayerState = new NormalPlayerState();
+        List<PlayerViewer> viewers = normalPlayerState.getViewer();
+        assertNotNull(viewers);
+        assertTrue(viewers.isEmpty());
+    }
+
+    @Test
+    void testGetControl() {
+        NormalPlayerState normalPlayerState = new NormalPlayerState();
+        List<PlayerController> controllers = normalPlayerState.getControl();
+        assertNotNull(controllers);
+        assertTrue(controllers.isEmpty());
+    }
+
+    @Test
+    void testGetModel() {
+        NormalPlayerState normalPlayerState = new NormalPlayerState();
+        List<Player> model = normalPlayerState.getModel();
+        assertNotNull(model);
+        assertTrue(model.isEmpty());
+    }
+
+    @Test
+    void testInitializing() {
+        NormalPlayerState normalPlayerState = new NormalPlayerState();
+        assertDoesNotThrow(() -> normalPlayerState.initializing());
+        List<Player> models = normalPlayerState.getModel();
+        assertNotNull(models);
+        assertEquals(2, models.size());
+    }
+}
