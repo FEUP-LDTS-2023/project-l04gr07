489e068d748125f726d0eb9473edbd8edd8a779b
beatrizbernardoo <beatrizxalexandra@gmail.com>
2023-12-09 22:32:58 +0000

    Added FruitTest

---
+++ src/test/java/l04gr07/model/FruitTest.java
@@ -0,0 +1,16 @@
+package l04gr07.model;
+
+import l04gr07.model.Game.FieldElements.Fruit;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FruitTest {
+    @Test
+    public void testGetPosition() {
+        Fruit fruit = new Fruit(4, 21);
+        Position position = fruit.getPosition();
+        assertEquals(4, position.getx());
+        assertEquals(21, position.gety());
+    }
+}
