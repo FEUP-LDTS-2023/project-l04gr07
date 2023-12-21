0a6aac18742ad734fdb143e65135b5c29f9b0942
Marta Silva <up202208258@fe.up.pt>
2023-12-21 17:17:16 +0000

    GameStateTest

--- src/test/java/l04gr07/states/GameStateTest.java
+++ src/test/java/l04gr07/states/GameStateTest.java
@@ -2,6 +2,9 @@ package l04gr07.states;
 
 import l04gr07.control.DifficultyController;
 import l04gr07.control.GameController;
+import l04gr07.gui.LanternGUI;
+import l04gr07.model.Game.Difficulty.EasyDifficulty;
+import l04gr07.model.Game.Difficulty.MediumDifficulty;
 import l04gr07.model.Game.GameModel;
 import l04gr07.model.Menu.DifficultyModel;
 import l04gr07.view.GameView.DifficultyView;
@@ -9,26 +12,59 @@ import l04gr07.view.GameView.GameView;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.awt.*;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
+
 
 public class GameStateTest {
     private GameState gameState;
+    private GameModel gameModel;
+    private GameView gameView;
+    private GameController gameController;
+    private LanternGUI guiMock;
+    private KeyboardSimulator keyboardSimulator;
 
     @BeforeEach
-    public void setUp() throws Exception{
-        gameState = new GameState();
+    public void setUp() throws IOException, URISyntaxException, FontFormatException {
+        gameModel = mock(GameModel.class);
+        gameView = mock(GameView.class);
+        gameController = mock(GameController.class);
+        guiMock = mock(LanternGUI.class);
+
+        gameState = new GameState(new EasyDifficulty());
+
+        when(guiMock.getScreen().pollInput()).thenAnswer(invocation -> {
+            return keyboardSimulator.getNextKey();
+        });
     }
 
     @Test
-    public void testGetters() throws Exception{
+    public void testGetters() throws IOException, URISyntaxException, FontFormatException {
         gameState.initializing(200);
+        assertNotNull(gameState);
+        assertNotNull(gameState.getModel());
         assertNotNull(gameState.getViewer());
         assertNotNull(gameState.getControl());
-        assertNotNull(gameState.getModel());
     }
 
-    //@Test
-    //test
-
+    @Test
+    public void testInitializing() throws IOException, URISyntaxException, FontFormatException {
+        gameState.initializing(200);
+        assertTrue(gameState.getModel() instanceof GameModel);
+        assertTrue(gameState.getViewer() instanceof GameView);
+        assertTrue(gameState.getControl() instanceof GameController);
+    }
 }
